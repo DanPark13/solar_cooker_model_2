3d2184ea6e8fb11b3f9513fe1ef46685300f9bbf
jsmilg <jsmilg@DXCSM53.olin.edu>
2020-11-09 11:32:48 -0500

    separate pot and temp

--- single_diameter_v2.m
+++ single_diameter_v2.m
@@ -1,4 +1,4 @@
-function [elapsedTime, waterTemperature] = single_diameter()
+function [elapsedTime, waterTemperature, potTemperature] = single_diameter_v2()
     %% DESCRIPTION sweep_diameter 
     % Performs a parameter sweep over the diameter of the mirror of the parabolic solar cooker
     
@@ -17,10 +17,11 @@ function [elapsedTime, waterTemperature] = single_diameter()
     mirror_area = pi * ((mirror_diameter/2)^2);
      
     % pot dimensions refer to inner dimensions
+    copperDensity = .892;            % kg/m^3
     potDiameter = 14/100;           % m
     potThickness = .5/100;          % m
     potHeight = 14/100;             % m
-    potConductivity = 5;          % thermal conductivity of pot walls (W/(m*K))
+    potConductivity = 5;            % thermal conductivity of pot walls (W/(m*K))
     potSpecificHeat = 380;          % specific heat of water (J/kg*K)
     waterSpecificHeat = 4186;       % specific heat of water (J/kg*K)
     waterDensity = 1;               % kg/L
@@ -30,29 +31,39 @@ function [elapsedTime, waterTemperature] = single_diameter()
     potOuterHeight = potHeight + 2*potThickness;
     
     potRadius = potDiameter/2;
+    
+    potVolume = pi*(potOuterDiameter/2)^2*potOuterHeight - pi*potRadius^2*potHeight;
+    potMass = potVolume*copperDensity;
+    
     waterHeight = .001 * waterVolume / (pi*potRadius^2);
     conductionArea = pi * potDiameter * waterHeight + potRadius^2;
     
-    convectionArea = 2*pi*(potDiameter/2)^2 + pi*potDiameter*potOuterHeight;
+    convectionArea = 2*pi*(potOuterDiameter/2)^2 + pi*potOuterDiameter*potOuterHeight;
     waterMass = waterDensity * waterVolume;
-    initialEnergy = temperatureToEnergy(initialTemperature, waterMass, waterSpecificHeat);
+    initialEnergyWater = temperatureToEnergy(initialTemperature, waterMass, waterSpecificHeat);
+    initialEnergyPot = temperatureToEnergy(initialTemperature, potMass, potSpecificHeat);
     
     %% Simulation Parameters
-    timeParams = [0, 30*60];   % convert the minutes to seconds
+    timeParams = [0, 10*60];   % convert the minutes to seconds
+    initialValues = [initialEnergyWater, initialEnergyPot];
     
     %% Simulation 
-    [T, U] = ode45(@heatFlow, timeParams, initialEnergy);
+    [T, U] = ode45(@heatFlow, timeParams, initialValues);
 
     elapsedTime = T./60;
-    waterTemperature = energyToTemperature(U, waterMass, waterSpecificHeat);
-    % potTemperature = energyToTemperature(U, potMass, potSpecificHeat);
-    % environmentLost
+    waterTemperature = energyToTemperature(U(:,1), waterMass, waterSpecificHeat);
+    potTemperature = energyToTemperature(U(:,2), potMass, potSpecificHeat);
     
-    function dUdt = heatFlow(~, U)
-        deltaTemp = energyToTemperature(U, waterMass, waterSpecificHeat) - airTemperature;
+    function res = heatFlow(~, U)
+        potTemp = energyToTemperature(U(2), potMass, potSpecificHeat);
+        waterTemp = energyToTemperature(U(1), waterMass, waterSpecificHeat);
+        deltaTempConvection = potTemp - airTemperature;
+        deltaTempConduction = potTemp - waterTemp;
         radiationRate = efficiency_of_Absorption * insolation * mirror_area;
-        % conductionRate = potConductivity * conductionArea / potThickness * deltaTemp;
-        convectionRate = 25*convectionArea * deltaTemp;
-        dUdt =  radiationRate - convectionRate;%conductionRate;
+        conductionRate = potConductivity * conductionArea / potThickness * deltaTempConduction;
+        convectionRate = 25*convectionArea * deltaTempConvection;
+        waterRate = conductionRate;
+        potRate = radiationRate - convectionRate;
+        res = [waterRate; potRate];
     end
 end
